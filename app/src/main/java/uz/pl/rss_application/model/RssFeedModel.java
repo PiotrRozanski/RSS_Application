96d2c5bbecb6427f1a56a134c88adb5476c846bf
piotr <87191@stud.uz.zgora.pl>
2017-05-16 18:07:28 +0200

    RSS-1 Dodanie parsera xml

---
+++ app/src/main/java/uz/pl/rss_application/model/RssFeedModel.java
@@ -0,0 +1,44 @@
+package uz.pl.rss_application.model;
+
+public class RssFeedModel {
+
+    private String title;
+    private String link;
+    private String description;
+
+    public RssFeedModel(String title, String link, String description) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+    }
+
+    public RssFeedModel() {
+        this.title = "";
+        this.link = "";
+        this.description = "";
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
