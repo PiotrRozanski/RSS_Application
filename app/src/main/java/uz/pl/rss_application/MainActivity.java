f876ee7da7dc31e4a97527699db39bc1b62a6a7a
Marta Mrugalska <marta.mrugalska@outlook.com>
2017-06-12 20:55:27 +0200

    fix konfliktu

--- app/src/main/java/uz/pl/rss_application/MainActivity.java
+++ app/src/main/java/uz/pl/rss_application/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
